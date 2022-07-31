b9048caf124a681c38d7d227d851e55c1ee91473
lorraynesanches <lorrainesanchesros@gmail.com>
2022-07-31 18:09:32 -0300

    first commit

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,45 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double a, b;
+
+        System.out.println("Digite o primeiro valor: ");
+        a = scan.nextDouble();
+        System.out.println("Digite o segundo valor: ");
+        b = scan.nextDouble();
+
+
+        double somar = somar(a, b);
+        double subtrair = subtrair(a, b);
+        double multiplicar = multiplicar(a, b);
+        double dividir = dividir(a, b);
+
+        System.out.println("somar: " + somar);
+        System.out.println("subt: " + subtrair);
+        System.out.println("mult: " + multiplicar);
+        System.out.println("div: " + dividir);
+
+    }
+
+    public static double somar(double a, double b) {
+
+        return a + b;
+    }
+    public static double subtrair(double a, double b) {
+
+        return a - b;
+    }
+    public static double multiplicar(double a, double b) {
+
+        return a * b;
+    }
+    public static double dividir(double a, double b) {
+
+        return a / b;
+    }
+}
